032a79f6881f1a88ecfb2bb2dee2b86e7fd7eb68
Lucas <lucashlfernandes33@gmail.com>
2020-08-24 18:04:07 -0300

    Salvando as os códigos num arquivo txt

--- Password/functions.py
+++ Password/functions.py
@@ -1,20 +1,31 @@
 import string
 import random
+from datetime import datetime
+
 
 def generateRandomPassword(lenght):
     passwordCharacters = string.ascii_letters + string.digits
-
     password = ''.join(random.choice(passwordCharacters) for i in range(lenght))
 
     return password
 
+
 def passwordsVerification(passwords, length):
     if length < 5 or length > 25:
         print("ERRO")
-    elif len(passwords) == 1:
-        print(f"{len(passwords)} senha gerada")
-        print(passwords)
+        return False
+    if len(passwords) == 1:
         return passwords
     else:
-        print(f"{len(passwords)} senhas geradas")
         return passwords
+
+
+def savePasswords(passwords):
+    now = datetime.now()
+    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+    archive = open('Senhas.txt', 'w')
+    z = 1
+    archive.write(f'\n \n{dt_string} \n')
+    for i in range(len(passwords)):
+        archive.write(f'Senhas {z}:' + passwords[i] + '\n')
+        z += 1
--- Password/main.py
+++ Password/main.py
@@ -1,4 +1,4 @@
-from functions import generateRandomPassword, passwordsVerification
+from Password.functions import generateRandomPassword, passwordsVerification, savePasswords
 
 passwords = []
 loop = int(input('Quantas senhas você quer gerar? \n'))
@@ -7,7 +7,21 @@ z = 1
 
 for i in range(loop):
     passwords.append(generateRandomPassword(length))
+    verify = passwordsVerification(passwords, length)
+    if verify == False:
+        break
     print(f'Senha {z}: {passwords[i]},')
     z += 1
+print(f'{len(passwords)} senhas geradas')
 
-passwordsVerification(passwords, length)
+if verify:
+    print('1- Salvar senhas em um arquivo')
+    print('2- Não salvar')
+    opcao = int(input())
+
+    if opcao == 1:
+        savePasswords(passwords)
+        print('Arquivo txt salvo na pasta atual')
+else:
+    print('Exit')
+    exit()
