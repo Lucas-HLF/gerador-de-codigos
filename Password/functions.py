213074390d00ed36dad4c2acf26663ee9bccae3f
Lucas <lucashlfernandes33@gmail.com>
2020-09-07 14:47:24 -0300

    Iterando os novos códigos no arquivo txt

--- Password/functions.py
+++ Password/functions.py
@@ -9,23 +9,13 @@ def generateRandomPassword(lenght):
 
     return password
 
-
-def passwordsVerification(passwords, length):
-    if length < 5 or length > 25:
-        print("ERRO")
-        return False
-    if len(passwords) == 1:
-        return passwords
-    else:
-        return passwords
-
-
 def savePasswords(passwords):
     now = datetime.now()
     dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-    archive = open('Senhas.txt', 'w')
     z = 1
+    archive = open('Senhas.txt', 'a+')
     archive.write(f'\n \n{dt_string} \n')
     for i in range(len(passwords)):
         archive.write(f'Senhas {z}:' + passwords[i] + '\n')
         z += 1
+    print("Novas senhas adicionandas no arquivo")
--- Password/main.py
+++ Password/main.py
@@ -1,27 +1,14 @@
-from Password.functions import generateRandomPassword, passwordsVerification, savePasswords
+from Password.functions import *
 
 passwords = []
 loop = int(input('Quantas senhas você quer gerar? \n'))
-length = int(input('Quantos caracteres a senha deve ter? (mínimo 5, máximo 25) \n'))
+length = int(input('Quantos caracteres a senha deve ter? \n'))
 z = 1
 
 for i in range(loop):
     passwords.append(generateRandomPassword(length))
-    verify = passwordsVerification(passwords, length)
-    if verify == False:
-        break
     print(f'Senha {z}: {passwords[i]},')
     z += 1
 print(f'{len(passwords)} senhas geradas')
 
-if verify:
-    print('1- Salvar senhas em um arquivo')
-    print('2- Não salvar')
-    opcao = int(input())
-
-    if opcao == 1:
-        savePasswords(passwords)
-        print('Arquivo txt salvo na pasta atual')
-else:
-    print('Exit')
-    exit()
+savePasswords(passwords)
